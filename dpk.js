2af103e97bb54433468db041a64abe0fd7658ede
Eweje Oluwafemi Williams <zwergywhite@yahoo.com>
2022-06-22 12:38:58 +0100

    update2

--- dpk.js
+++ dpk.js
@@ -1,30 +1,13 @@
 const crypto = require("crypto");
 
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let candidate = '0';
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
+  if (event) {    
       candidate = crypto.createHash("sha3-512")
-        .update(data)
-        .digest("hex");
-    }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
+        .update(event)
+        .digest("hex");    
+  }  
+  
   return candidate;
 };
