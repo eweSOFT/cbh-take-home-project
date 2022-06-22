ba36ed080d773e3b73a71701b468d6c3281a794a
Eweje Oluwafemi Williams <zwergywhite@yahoo.com>
2022-06-22 15:36:20 +0100

    update7

--- dpk.test.js
+++ dpk.test.js
@@ -6,3 +6,10 @@ describe("deterministicPartitionKey", () => {
     expect(trivialKey).toBe("0");
   });
 });
+
+describe("deterministicPartitionKey(0)", () => {
+  it("Returns the hashed value when digit zero (0) is given", () => {
+    const n = deterministicPartitionKey(0);
+    expect(n).toBeTruthy();
+  });
+});
